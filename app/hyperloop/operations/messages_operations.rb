d86282ad55c78d9bb5c2d7430d396a68983f90aa
Billy.Zheng <zw963@163.com>
2017-07-25 12:13:49 +0800

    添加 Message Operations 下面的 GetMessage:
    这个 Operation 用来从 Rails.cache 获取之前存储的 cache.

--- app/hyperloop/operations/messages_operations.rb
+++ app/hyperloop/operations/messages_operations.rb
@@ -10,4 +10,15 @@ module MessagesOperations
       Rails.cache.write('messages', newcachedmessages)
     end
   end
+
+  class GetMessages < Hyperloop::ServerOp
+    param :acting_user, nils: true
+    dispatch_to { Hyperloop::Application }
+
+    # 下面的 outbound 的特殊声明, 其实是声明了 Store 下面的一个同名的 State.
+    # 而之后对 params.messages 的操作, 其实是在操作 Store
+    outbound :messages
+
+    step { params.messages = Rails.cache.fetch('messages') { [] } }
+  end
 end
